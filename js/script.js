60d99e3b8aa09c678c40bceda669e3b28078a102
rafidina <rafi.dina240123@gmail.com>
2024-07-18 21:41:48 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const btnLogin = document.querySelector('#btnLogin');
+const pesan = document.querySelector('#pesan');
+let database = [
+    {
+        'user' : 'sayang',
+        'pass' : '210703'
+    },
+    {
+        'user' : 'Sayang',
+        'pass' : '210703'
+    }
+]
+btnLogin.addEventListener('click',()=>{
+    pesan.innerHTML = '';
+    let user = username.value;
+    let pass = password.value;
+    let validate = true;
+    if (user == ''){
+        validate = false;
+        pesan.innerHTML += '<div class="alert alert-danger">Username tidak boleh kosong</div>';
+    }
+    if (pass == ''){
+        validate = false;
+        pesan.innerHTML += '<div class="alert alert-danger">Password tidak boleh kosong</div>';
+    }
+    if (validate) {
+        let success = {'user' : false, 'pass' : false};
+        for (let i = 0; i < database.length; i++) {
+            if (user == database[i]['user']) {
+                success.user = true;
+                if (pass == database[i]['pass']) {
+                    success.pass = true;
+                }
+            }
+        }
+        if (success.user) {
+            if (success.pass) {
+                document.location.href = 'index.html';
+            }else{
+                pesan.innerHTML += '<div class="alert alert-danger">Password salah</div>';
+            }
+        }else{
+            pesan.innerHTML += '<div class="alert alert-danger">Username tidak dikenali</div>';
+        }
+    }
+
+});
